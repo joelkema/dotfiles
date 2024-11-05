cb2a514ef7e83404da4d2ec454b0c9f6bc8dc67d
Joël Kema <jkema@anwb.nl>
2024-11-05 08:12:42 +0100

    change tmux-sessionizer and add resize pane to tmux.conf

--- nvim/.config/nvim/lua/joelkema/settings.lua
+++ nvim/.config/nvim/lua/joelkema/settings.lua
@@ -6,8 +6,8 @@ vim.opt.tabstop = 4
 vim.api.nvim_set_keymap('n', '<leader><Tab>', ':tabn<CR>', { noremap = true, silent = true })
 
 -- Copilot smartindent
-vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<Tab>")', { silent = true, expr = true })
+-- vim.g.copilot_no_tab_map = true
+-- vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<Tab>")', { silent = true, expr = true })
 
 -- VSCode keybindings
 -- vim.api.nvim_set_keymap("n", "<F2>", ":lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
