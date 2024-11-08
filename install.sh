7efd47afba7617ccc11cbdc3be1b80552e92a37c
Joel <jkema@anwb.nl>
2024-11-08 14:12:27 +0100

    add git add user step

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ brew tap Homebrew/bundle
 # Install Brewfile
 brew bundle install
 
+# Set git user
+[ -z `git config --global user.name` ] && git config --global user.name "Joel Kema"
+[ -z `git config --global user.email` ] && git config --global user.email "joelkema1988@gmail.com"
+
 # Set up symlinks using stow
 source symlinks.sh
 
