885536d9addc84b1df71fac210fe63701b3eab5d
Joel <jkema@anwb.nl>
2024-11-11 11:14:25 +0100

    add oh my zsh install + adopt strategy for stow (replacing .zshrc)

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ brew bundle install
 [ -z `git config --global user.name` ] && git config --global user.name "Joel Kema"
 [ -z `git config --global user.email` ] && git config --global user.email "joelkema1988@gmail.com"
 
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Set up symlinks using stow
 source symlinks.sh
 
--- symlinks.sh
+++ symlinks.sh
@@ -2,5 +2,5 @@
 
 for d in `ls -d */`;
 do
-    ( stow --restow $d )
+    ( stow --restow --adopt $d )
 done
